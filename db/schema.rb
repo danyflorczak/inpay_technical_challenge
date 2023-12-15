4b4d70f22b9eca0852ab6ead9f5ee93381c53c01
Daniel Florczak <danyflorczak@gmail.com>
2023-12-15 14:54:08 +0100

    add oauth fields to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_15_130911) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_15_135323) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,10 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_15_130911) do
     t.string "encrypted_password", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "token"
+    t.string "refresh_token"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
