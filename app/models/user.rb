5b9891382d9da494629474284c385e3edd0b926e
Daniel Florczak <danyflorczak@gmail.com>
2023-12-16 14:08:05 +0100

    adjust devise to logging only via google

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ApplicationRecord
   has_many :emails, dependent: :destroy
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable,
-    :omniauthable, omniauth_providers: [:google_oauth2]
+  devise :omniauthable, omniauth_providers: [:google_oauth2]
 
   def self.from_omniauth(access_token)
     find_or_create_by(provider: access_token.provider, email:
