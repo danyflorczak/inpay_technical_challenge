9bd892b1f33ccb2fc0382b3cb46993956f2d3895
Daniel Florczak <71887549+danyflorczak@users.noreply.github.com>
2023-12-17 19:50:35 +0100

    Error handling (#12)
    * include view with error messages
    
    * add error handling to emails controller

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,4 +1,6 @@
 class EmailsController < ApplicationController
+  before_action :set_ransack_query, only: [:count, :subjects]
+
   def index
   end
 
@@ -7,22 +9,32 @@ class EmailsController < ApplicationController
   end
 
   def count
-    @q = current_user.emails.ransack(params[:q])
     @emails_count = params[:q] ? @q.result.count : nil
+  rescue => e
+    handle_error(e, "counting emails")
   end
 
   def subjects
-    @q = current_user.emails.ransack(params[:q])
     @subjects = params[:q] ? @q.result.pluck(:subject) : []
+  rescue => e
+    handle_error(e, "retrieving subjects")
   end
 
   def stats
-    @most_frequent_sender = current_user.emails.group(:sender).order("count_sender desc").count("sender").first
+    @most_frequent_sender = current_user.emails.group(:sender)
+      .order("count_sender desc")
+      .count("sender").first
+  rescue => e
+    handle_error(e, "retrieving stats")
   end
 
   def sync_emails
     if user_signed_in?
-      EmailSyncJob.perform_later(current_user)
+      begin
+        EmailSyncJob.perform_later(current_user)
+      rescue => e
+        handle_error(e, "syncing emails")
+      end
     end
   end
 
@@ -30,9 +42,20 @@ class EmailsController < ApplicationController
   end
 
   def perform_sync_with_date_range
-    start_date = params[:start_date]
-    end_date = params[:end_date]
-    EmailSyncJob.perform_later(current_user, start_date, end_date)
+    EmailSyncJob.perform_later(current_user, params[:start_date], params[:end_date])
     redirect_to sync_emails_emails_path
+  rescue => e
+    handle_error(e, "syncing emails for date range")
+  end
+
+  private
+
+  def set_ransack_query
+    @q = current_user.emails.ransack(params[:q])
+  end
+
+  def handle_error(exception, action)
+    flash[:error] = "Error #{action}: #{exception.message}"
+    redirect_to dashboard_emails_path
   end
 end
