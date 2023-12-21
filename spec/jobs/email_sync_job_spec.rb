34b6bf530df9020470842802b5361e083cfeeafb
Daniel Florczak <danyflorczak@gmail.com>
2023-12-22 00:52:30 +0100

    adjusting specs to new changes in services and jobs

--- spec/jobs/email_sync_job_spec.rb
+++ spec/jobs/email_sync_job_spec.rb
@@ -5,75 +5,41 @@ RSpec.describe EmailSyncJob do
 
   let(:user) { create(:user) }
   let(:email_retrieval_service) { instance_double(EmailRetrievalService) }
-  let(:email_processing_service) { instance_double(EmailProcessingService) }
+  let(:email_processing_service) { instance_double(EmailProcessingService, process_and_save_emails: {success: true, message: "Successfully saved all mail records"}) }
   let(:emails) { [double("email")] }
 
   before do
+    allow(User).to receive(:find).with(user.id).and_return(user)
     allow(EmailRetrievalService).to receive(:new).with(user).and_return(email_retrieval_service)
     allow(EmailProcessingService).to receive(:new).with(user).and_return(email_processing_service)
+    allow(email_retrieval_service).to receive(:fetch_emails).and_return(emails)
+    allow(EmailSyncLoggerService).to receive(:log)
+    allow(EmailSyncBroadcasterService).to receive(:broadcast)
   end
 
   describe "#perform" do
-    context "when no date range is provided" do
-      before do
-        allow(email_retrieval_service).to receive(:fetch_all_emails).and_return(emails)
-        allow(email_processing_service).to receive(:process_and_save_emails).with(emails).and_return({success: "Successfully saved all mail records"})
-      end
+    let(:start_date) { "2023-01-01" }
+    let(:end_date) { "2023-01-31" }
 
-      it "fetches all emails" do
-        perform_enqueued_jobs { described_class.perform_later(user) }
-        expect(email_retrieval_service).to have_received(:fetch_all_emails)
-      end
-
-      it "processes the fetched emails" do
-        perform_enqueued_jobs { described_class.perform_later(user) }
-        expect(email_processing_service).to have_received(:process_and_save_emails).with(emails)
-      end
+    it "fetches emails without a date range" do
+      perform_enqueued_jobs { described_class.perform_later(user.id) }
+      expect(email_retrieval_service).to have_received(:fetch_emails).with(nil, nil)
     end
 
-    context "when a date range is provided" do
-      let(:start_date) { "2023-01-01" }
-      let(:end_date) { "2023-01-31" }
-
-      before do
-        allow(email_retrieval_service).to receive(:fetch_emails_in_date_range).with(start_date, end_date).and_return(emails)
-        allow(email_processing_service).to receive(:process_and_save_emails).with(emails).and_return({success: "Successfully saved all mail records"})
-      end
-
-      it "fetches emails within the date range" do
-        perform_enqueued_jobs { described_class.perform_later(user, start_date, end_date) }
-        expect(email_retrieval_service).to have_received(:fetch_emails_in_date_range).with(start_date, end_date)
-      end
-
-      it "processes the fetched emails" do
-        perform_enqueued_jobs { described_class.perform_later(user, start_date, end_date) }
-        expect(email_processing_service).to have_received(:process_and_save_emails).with(emails)
-      end
+    it "fetches emails within a date range" do
+      perform_enqueued_jobs { described_class.perform_later(user.id, start_date, end_date) }
+      expect(email_retrieval_service).to have_received(:fetch_emails).with(start_date, end_date)
     end
 
-    context "when email processing fails" do
-      before do
-        allow(email_retrieval_service).to receive(:fetch_all_emails).and_return(emails)
-        allow(email_processing_service).to receive(:process_and_save_emails).with(emails).and_return({error: "Failed to save emails"})
-      end
-
-      it "logs an error" do
-        allow(Rails.logger).to receive(:error)
-        perform_enqueued_jobs { described_class.perform_later(user) }
-        expect(Rails.logger).to have_received(:error).with("Failed to process emails for user #{user.id}")
-      end
+    it "processes the fetched emails" do
+      perform_enqueued_jobs { described_class.perform_later(user.id) }
+      expect(email_processing_service).to have_received(:process_and_save_emails).with(emails)
     end
 
     context "when email processing is successful" do
-      before do
-        allow(email_retrieval_service).to receive(:fetch_all_emails).and_return(emails)
-        allow(email_processing_service).to receive(:process_and_save_emails).with(emails).and_return({success: "Successfully saved all mail records"})
-      end
-
       it "broadcasts a completion status" do
-        allow(ActionCable.server).to receive(:broadcast)
-        perform_enqueued_jobs { described_class.perform_later(user) }
-        expect(ActionCable.server).to have_received(:broadcast).with("email_sync_user_#{user.id}", {status: "completed"})
+        perform_enqueued_jobs { described_class.perform_later(user.id) }
+        expect(EmailSyncBroadcasterService).to have_received(:broadcast).with(user)
       end
     end
   end
